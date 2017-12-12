9f1a782946283d1fa83ce82c6e8cbab3bffcf8b7
lbrande <lovbra00@gmail.com>
2017-12-12 17:07:15 +0100

    Added random.

--- src/TimerTemplate.java
+++ src/TimerTemplate.java
@@ -2,6 +2,7 @@ import java.io.IOException;
 import java.io.OutputStreamWriter;
 import java.io.PipedInputStream;
 import java.io.PipedOutputStream;
+import java.util.Random;
 import java.util.concurrent.Executor;
 import java.util.concurrent.Executors;
 
@@ -18,6 +19,7 @@ public class TimerTemplate {
     OutputStreamWriter out =
         new OutputStreamWriter(new PipedOutputStream((PipedInputStream) System.in));
     StringBuilder input = new StringBuilder();
+    Random random = new Random();
 
     out.write(input.toString());
     executor.execute(runner);
