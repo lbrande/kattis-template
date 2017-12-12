f36f81ed6a087d49a8245ccad23f240bdec1e3bc
lbrande <lovbra00@gmail.com>
2017-12-12 14:33:15 +0100

    First commit.

---
+++ src/Template.java
@@ -0,0 +1,7 @@
+public class Template {
+  public static void main(String[] args) {
+    Kattio io = new Kattio(System.in);
+
+    io.close();
+  }
+}
---
+++ src/TimerTemplate.java
@@ -0,0 +1,25 @@
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.io.PipedInputStream;
+import java.io.PipedOutputStream;
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
+
+public class TimerTemplate {
+  public static void main(String[] args) throws IOException {
+    Executor executor = Executors.newSingleThreadExecutor();
+    Runnable runner =
+        () -> {
+          long startTime = System.currentTimeMillis();
+          Template.main(null);
+          System.out.println("Execution took " + (System.currentTimeMillis() - startTime) + " ms");
+        };
+    System.setIn(new PipedInputStream());
+    OutputStreamWriter out =
+        new OutputStreamWriter(new PipedOutputStream((PipedInputStream) System.in));
+    StringBuilder input = new StringBuilder();
+
+    out.write(input.toString());
+    executor.execute(runner);
+  }
+}
